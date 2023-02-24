06e0894173a2acac714c385b59e16b64c7f84cee
taleshrocha <taleshrochaz@gmail.com>
2023-02-24 17:58:51 -0300

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 export default function Header() {
   return (
-    <nav className="flex fixed top-0 z-20 w-screen h-16 bg-sky-900">
-      
+    <nav className="flex justify-between items-center px-4
+      fixed top-0 z-20 w-screen h-16 bg-sky-500">
+      <p className="font-bold text-white text-4xl">Ecomer</p>
     </nav>
   );
 }
